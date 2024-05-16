12eceda820c858bdafc9c47619c16fc1df40aaa4
Prof-Ursi <8743@holbertonstudents.com>
2024-05-16 17:08:41 +0200

    Correcting Output error

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    reverse_list = my_list[::-1]
-    for i in range(len(reverse_list)):
-        print(reverse_list[i])
+    if my_list is None:
+        return None
+    else:
+        my_list.reverse()
+    for i in range(len(my_list)):
+        print(my_list[i])
