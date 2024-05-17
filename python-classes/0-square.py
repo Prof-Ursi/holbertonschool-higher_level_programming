d872cba01128d953de904486efb7a97e087e1b7f
Prof-Ursi <8743@holbertonstudents.com>
2024-05-17 17:30:22 +0200

    Adding 0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Square Class
+
+This class will be used to create square. Soon. Very soon. But not now.
+
+"""
+
+
+class Square:
+    """See ? This is empty. Go back later for some square witchcraft."""
+    pass
