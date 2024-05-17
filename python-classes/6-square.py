6a36d47b90d2c606db15d8e7d911e224f2e652ee
Prof-Ursi <8743@holbertonstudents.com>
2024-05-17 20:50:19 +0200

    Fixing Output

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -114,8 +114,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(self.position[1]):
+            for i in range(self.__position[1]):
                 print()
             for j in range(self.__size):
-                print(" " * self.position[0], end="")
+                print(" " * self.__position[0], end="")
                 print("#" * self.__size)
