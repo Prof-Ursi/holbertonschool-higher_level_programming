7f70fe41bf8d8b171e96d277e756424a32cbd6be
Prof-Ursi <8743@holbertonstudents.com>
2024-05-17 16:53:59 +0200

    Adding 2-safe_print_list_integers.py  , working locally & pycodestyle approved

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    int_printed = 0
+    for i in range(x):
+        try:
+            if type(my_list[i]) is int:
+                print('{:d}'.format(my_list[i]), end="")
+                int_printed += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            break
+    print("")
+    return int_printed
