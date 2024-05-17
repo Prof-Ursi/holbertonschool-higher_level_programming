bd22989cdea67728fc6bbf15853f0ba78730e352
Prof-Ursi <8743@holbertonstudents.com>
2024-05-17 20:42:13 +0200

    Updating 5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -31,18 +31,8 @@ class Square:
         Attributes :
             __size (int): The size of the square
 
-        Raises :
-            TypeError : If 'size' type is not 'int'.
-
-            ValueError : If 'size' value is strictly less than 0.
-
         """
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = size
+        self.__size = size
 
     @property
     def size(self):
