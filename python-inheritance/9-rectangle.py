29b056bffa091b09c9aad7e22c634d377a401608
Prof-Ursi <8743@holbertonstudents.com>
2024-05-25 15:08:26 +0200

    Adding everything, working locally & pycodestyle approved

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""
+This module defines the `BaseGeometry` class and the `Rectangle` class.
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Initializes a new Rectangle instance.
+
+    Attributes:
+        __width (int): the width of the rectangle.
+        __height (int): the height of the rectangle.
+
+    Methods:
+        __init__(width, height): Initializes a new Rectangle
+        instance with the specified width and height.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialize the new Rectangle.
+
+        Args:
+            - width (int): the width of the rectangle.
+            - height (int): the height of the rectangle.
+
+        Raises:
+            - TypeError: If either width or height is not an integer.
+            - ValueError: If either width or height is less than or equal to 0.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Calculate and return the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """
+        Return a string representation of the rectangle.
+
+        Returns:
+            str: a string representation of the rectangle.
+        """
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
