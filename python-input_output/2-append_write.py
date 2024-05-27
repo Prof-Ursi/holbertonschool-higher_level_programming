6ca9a5ea44da17368427f1d64e64671a78cd36ed
Prof-Ursi <8743@holbertonstudents.com>
2024-05-27 13:51:40 +0200

    Adding 2-append_write.py, working locally & pycodestyle approved

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module contains a function that appends a string at the end of
+a text file and returns the number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends the given string to the end of a text file
+    and returns the number of characters added.
+
+    Args:
+        filename (str): the name of the file
+                        to which the text will be appended.
+        text (str): the text to be appended to the file.
+
+        Returns
+        number of characters written through the intern return
+        of the write() syscall.
+    """
+    with open(filename, 'a', encoding="utf-8") as file:
+        return (file.write(text))
