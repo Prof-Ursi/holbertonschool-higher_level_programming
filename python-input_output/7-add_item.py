ef38c009bbbfd9c3854e2892c410134980d00848
Prof-Ursi <8743@holbertonstudents.com>
2024-05-31 21:18:32 +0200

    Adding everything else

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,6 +5,8 @@ to a Python list, and then save them to a file.
 """
 import os
 import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 file_name = "add_item.json"
--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 This module defines a function that returns the dictionary description
 with simple data structure for JSON serialization of an object.
