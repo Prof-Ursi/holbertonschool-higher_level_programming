87a6bed55349781fd6e7994c18c09e27011cedab
Prof-Ursi <8743@holbertonstudents.com>
2024-05-31 21:57:34 +0200

    Adding task_03_http_server.py, working locally & pycodestyle approved

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+'''
+This program sets up a simple API using http.server python module.
+'''
+import http.server
+import json
+import socketserver
+
+
+class Handler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        '''
+        Modification of the do_GET method in 'http.server' module.
+        Returns a simple dataset example.
+        '''
+        if self.path == '/data':
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+        elif self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(b'OK')
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            self.wfile.write(json.dumps(info).encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'404 Not Found')
+
+
+PORT = 8000
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
