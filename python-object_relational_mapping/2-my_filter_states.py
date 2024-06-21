d0f374718dc01ad6b6da9c48324f37022f3f9cd1
Prof-Ursi <8743@holbertonstudents.com>
2024-06-21 17:40:00 +0200

    Adding 2-my_filter_states.py to repo, working locally & pycodestyle approved

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that display all values in the states table
+of 'hbtn_0e_0_usa' where 'name' matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states
+                      WHERE name LIKE BINARY '{}'
+                      ORDER BY states.id
+                   """.format(sys.argv[4]))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
