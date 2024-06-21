2510ddd170e950a9b660ad6f3ef491d0402ddb46
Prof-Ursi <8743@holbertonstudents.com>
2024-06-21 13:46:00 +0200

    Updating 9-full_creation.sql

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- Create a table `second_table` in the database `hbtn_0c_0` in the MySQL server and add multiples rows.
+CREATE TABLE IF NOT EXISTS second_table (
+	id int,
+	name varchar(256),
+	score int
+);
+
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
