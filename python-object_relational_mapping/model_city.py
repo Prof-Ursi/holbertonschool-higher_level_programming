9a0be55da2582311860e2c0d8a834cf74474f0af
Prof-Ursi <8743@holbertonstudents.com>
2024-06-21 19:06:00 +0200

    Updating model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """
-Module that defines the `State` class and initializes a `Base` object.
+Module that defines the 'City' class and initializes a 'Base' object.
 """
-from sqlalchemy import Column, Integer, String
+
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class State(Base):
+class City(Base):
     """
-    Represents a state for a MySQL database.
+    Represents a city for a MySQL database.
 
     Attributes:
         - id (int): an auto-generated, unique integer
         representing the state's ID.
         - name (str): a string with a maximum of
-        128 characters representing the state's name.
+        128 characters representing the city's name.
     """
-    __tablename__ = "states"
+    __tablename__ = "cities"
     id = Column(
         Integer,
         primary_key=True,
@@ -27,3 +28,8 @@ class State(Base):
         unique=True
         )
     name = Column(String(128), nullable=False)
+    state_id = Column(
+        Integer,
+        ForeignKey("states.id", ondelete="CASCADE"),
+        nullable=False
+        )
