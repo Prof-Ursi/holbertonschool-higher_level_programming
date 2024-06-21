6efbe4f06ff9ceb20ca8994f5ac2e568d1937c0a
Prof-Ursi <8743@holbertonstudents.com>
2024-06-21 14:05:27 +0200

    Updating 15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,6 +1,5 @@
 -- List the number of records with the same score in the table `second_table` of the database `hbtn_0c_0` in the MySQL server.
-SELECT COUNT(score)
-AS number
+SELECT score, COUNT(score) AS number
 FROM second_table
 GROUP BY score
 ORDER BY number DESC;
