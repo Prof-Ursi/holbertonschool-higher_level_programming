61a847a4709707bb20c0f1c83f634e7684b405ad
Prof-Ursi <8743@holbertonstudents.com>
2024-06-21 17:19:05 +0200

    Updating 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module that lists all states from the database hbtn_0e_0_usa.
+Import the MySQLdb and sys module
 """
 import MySQLdb
 import sys
@@ -11,12 +11,10 @@ if __name__ == "__main__":
         host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
         port=3306
     )
     cursor = db.cursor()
     cursor.execute("SELECT states.id, states.name FROM states ORDER BY id ASC")
-    query_rows = cursor.fetchall()
-    for row in query_rows:
-        print(row)
-    cursor.close()
+    for i in cursor:
+        print(i)
